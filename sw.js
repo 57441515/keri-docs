83e9d660df21eb4fe3b58a6316d70b152cdfc093
57441515 <mfdaza1@gmail.com>
2026-06-14 15:36:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,22 @@
-const CACHE = 'keri-docs-v1';
-const FILES = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE = 'keri-docs-v2';
+const ARCHIVOS = [
+  '/keri-docs/',
+  '/keri-docs/index.html',
+  '/keri-docs/manifest.json',
+  '/keri-docs/icono-192.png',
+  '/keri-docs/icono-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html'))));
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/keri-docs/index.html'))));
 });
